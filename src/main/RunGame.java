e9d128d68374f4bed2bc1c5aaba1ea389fe7f480
karthshen <karth-shen@hotmail.com>
2017-03-25 23:00:41 -0400

    The structures for the view, along with some standlone classes

--- src/main/RunGame.java
+++ src/main/RunGame.java
@@ -1,5 +1,10 @@
+import javafx.application.Application;
+
 /**
  * Created by cduica on 3/22/17.
  */
 public class RunGame {
+    public static void main(String []args){
+        Application.launch(GameEngine.class, args);
+    }
 }
---
+++ src/main/view/Scene/mapMakerPanel/MapPanel.java
@@ -0,0 +1,4 @@
+package view.Scene.mapMakerPanel;
+
+public class MapPanel {
+}
