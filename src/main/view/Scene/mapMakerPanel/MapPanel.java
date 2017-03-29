9e3291c88ba31c508ce08f678f9083f63cbb24f9
Allison Aguirre <allisonaguirre7106@gmail.com>
2017-03-29 02:25:16 -0400

    fix map panel

--- src/main/view/Scene/mapMakerPanel/MapPanel.java
+++ src/main/view/Scene/mapMakerPanel/MapPanel.java
@@ -20,6 +20,7 @@ import model.TileSubsystem.Terrains.Rock;
 import model.TileSubsystem.Terrains.Sea;
 import model.TileSubsystem.Tiles.LandTile;
 import model.TileSubsystem.Tiles.RiverTile;
+import model.TileSubsystem.Tiles.SeaTile;
 import model.TileSubsystem.Tiles.Tile;
 import model.TileSubsystem.Visitor.TileDrawingVisitor;
 import model.TileSubsystem.Visitor.TileVisitor;
@@ -58,7 +59,7 @@ public class MapPanel extends Panel{
     private HashMap<Location, Tile> generateMap(){
         LandTile mountainTile = new LandTile(Mountains.getInstance());
         LandTile rockTile = new LandTile(Rock.getInstance());
-        LandTile seaTile = new LandTile(Sea.getInstance());
+        SeaTile seaTile = new SeaTile(Sea.getInstance());
 
         NormalRiver river = new NormalRiver(HexSide.N ,HexSide.NE);
         ForkedRiver riverF = new ForkedRiver(HexSide.SE , HexSide.N, HexSide.SW);
