651b1d8fdc9c642ed4a75a89b5ae8a2826f54c2e
Allison Aguirre <allisonaguirre7106@gmail.com>
2017-04-11 23:17:46 -0400

    build abilities framework

---
+++ src/main/model/Abilities/BuildAbility.java
@@ -0,0 +1,8 @@
+package model.Abilities;
+
+/**
+ * Created by allisonaguirre on 4/11/17.
+ */
+public abstract class BuildAbility {
+    public abstract void addToPlayerAbilityAvailabilityList(PlayerAbilityAvailability list);
+}
