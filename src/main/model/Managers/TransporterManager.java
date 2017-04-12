1ea14e312b0f1ff0ae1f9bd2ca87d0e137c60a26
hkerins <hkerins@ufl.edu>
2017-04-11 23:06:29 -0400

    changed producer managers

--- src/main/model/Managers/TransporterManager.java
+++ src/main/model/Managers/TransporterManager.java
@@ -1,14 +1,51 @@
 package model.Managers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by hankerins on 4/8/17.
- * TODO: separate list managers and object managers
  */
-public abstract class TransporterManager<Obj, Loc> extends TransporterAndProducerManager<Obj, Loc> {
+public abstract class TransporterManager<Obj, Loc>{
+    private HashMap<Obj, Loc> managerMap = new HashMap<Obj, Loc>();
+
+    protected HashMap<Obj, Loc> getManagerMap() {
+        return managerMap;
+    }
+
+    public void add(Obj o, Loc loc)
+
+    {
+        managerMap.put(o, loc);
+    }
+
+    public void removeOccupant(Obj o){
+        managerMap.remove(o);
+    }
+
+    public Loc getLocation(Obj o)
+    {
+        return managerMap.get(o);
+    }
+
+    public boolean containsKey(Obj o){
+        return managerMap.containsKey(o);
+    }
+
+    public ArrayList<Obj> getContentsOfArea(Loc l){
+        ArrayList<Obj> contents = new ArrayList<Obj>();
+        for (Obj o: managerMap.keySet()){
+            if(managerMap.get(o) == l){
+                contents.add(o);
+            }
+        }
+        return contents;
+    }
 
     public void move(Obj o, Loc newLoc){
         removeOccupant(o);
         add(o, newLoc);
     }
+
 
 }
