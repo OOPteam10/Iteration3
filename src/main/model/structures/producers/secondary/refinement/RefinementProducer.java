fbe8d4dd8b77e257e7a8b5b64bc322d213ff7500
hkerins <hkerins@ufl.edu>
2017-04-12 23:23:56 -0400

    what

--- src/main/model/structures/producers/secondary/refinement/RefinementProducer.java
+++ src/main/model/structures/producers/secondary/refinement/RefinementProducer.java
@@ -10,5 +10,5 @@ import model.structures.producers.secondary.SecondaryProducer;
  * Created by cduica on 4/8/17.
  */
 public abstract class RefinementProducer extends SecondaryProducer {
-    
+
 }
