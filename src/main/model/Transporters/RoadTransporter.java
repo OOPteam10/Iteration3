ba3a92ce4e9d04169688bbad0e48d7c553f209b0
karthshen <karth-shen@hotmail.com>
2017-04-13 00:15:42 -0400

    RoadTransporter extends Transporter

--- src/main/model/Transporters/RoadTransporter.java
+++ src/main/model/Transporters/RoadTransporter.java
@@ -1,11 +1,16 @@
 package model.Transporters;
 
 import model.Managers.LandTransporterManager;
+import model.TileSubsystem.Sector;
 import model.Transporters.Visitor.RoadTransporterVisitor;
 
 /**
  * Created by hankerins on 4/8/17.
  */
-public abstract class RoadTransporter extends Transporter{
+public abstract class RoadTransporter extends Transporter<Sector>{
+    public void dropOff(Sector s){
+        
+    }
     public abstract void accept(RoadTransporterVisitor v);
+
 }
