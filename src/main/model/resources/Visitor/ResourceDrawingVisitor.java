cf397873723d56d7c8a9216679da698511060fba
karthshen <karth-shen@hotmail.com>
2017-04-15 00:10:03 -0400

    Visitor for drawing resources

---
+++ src/main/model/resources/Visitor/ResourceDrawingVisitor.java
@@ -0,0 +1,55 @@
+package model.resources.Visitor;
+
+import javax.annotation.Resource;
+
+public class ResourceDrawingVisitor implements ResourceVisitor {
+    @Override
+    public void visitBoard() {
+
+    }
+
+    @Override
+    public void visitClay() {
+
+    }
+
+    @Override
+    public void visitCoin() {
+
+    }
+
+    @Override
+    public void visitFuel() {
+
+    }
+
+    @Override
+    public void visitGold() {
+
+    }
+
+    @Override
+    public void visitIron() {
+
+    }
+
+    @Override
+    public void visitPaper() {
+
+    }
+
+    @Override
+    public void visitStock() {
+
+    }
+
+    @Override
+    public void visitStone() {
+
+    }
+
+    @Override
+    public void visitTrunk() {
+
+    }
+}
